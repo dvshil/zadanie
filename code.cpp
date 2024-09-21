48bd3ad416a266b9a2f227a1256ba9698b4684db
dvshil <sadhbmm@gmail.com>
2024-09-21 19:13:37 +0300

    add the input of two numbers

--- code.cpp
+++ code.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <stdio.h>
 
 int main() {
-	std::cout << "Hello\n";
+	int a, b;
+
+	printf("Enter the first number:\n");
+	scanf_s("%d", &a);
+
+	printf("Enter the second number:\n");
+	scanf_s("%d", &b);
+
+	return 0;
 }
